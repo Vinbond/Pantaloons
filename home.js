f7400cdc23982c4bdcd635f5d7aef3e577fcefbc
vinbond <vinayakkundangar77@gmail.com>
2026-02-20 17:56:40 +0530

    restart

--- home.js
+++ home.js
@@ -1,9 +1,36 @@
-let x="10";
-console.log(typeof(x))
-let y=+x;
-console.log(typeof(y))
-console.log(y)
-let z=-y;
-console.log(z)
-let w=~z;
-console.log(w)
+// let x="10";
+// console.log(typeof(x))
+// let y=+x;
+// console.log(typeof(y))
+// console.log(y)
+// let z=-y;
+// console.log(z)
+// let w=~z;
+// console.log(w)
+// function fun(){
+//     $("#img").fadeToggle();
+// }
+// $("#ptn").click(function(){
+//     $("#img").slideDown();
+//   });
+// $("#ptn").click(function(){
+//     $("#img").slideUp();
+//   });
+// let name=prompt("Enter name :")
+// let ID=parseInt(prompt("Enter ID :"))
+// let salary=parseFloat(prompt("Enter salary :"))
+// console.log(name)
+// console.log(ID)
+// console.log(salary)
+// obg={
+//   name=name
+//   ID
+//   salary
+// }
+//abstraction
+// class Car{
+//    clutch=false;
+//    gear=false;
+//    start=false;
+//   }
+
